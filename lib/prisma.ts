7810f0868f26378e13317d2c45263988b8212d7a
Zeeshan-1124 <imranaliwarsi707@gmail.com>
2026-07-19 13:35:06 +0530

    Updated prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({ log: ['error'] });
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({ log: ['error'] });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { neonConfig } from '@neondatabase/serverless'
+import ws from 'ws'
+
+neonConfig.webSocketConstructor = ws
+
+const connectionString = process.env.DATABASE_URL!
+const adapter = new PrismaNeon({ connectionString })
+
+export const prisma = new PrismaClient({ adapter })
